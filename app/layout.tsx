f3fff71e1c7b887ad9ae6cfc23b41057b6880f1f
Alexey Alexeev <alexeevv-alexey@yandex.ru>
2025-10-19 23:07:24 +0300

    обновил favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://gqnwyyinswqoustiqtpk.supabase.co" />
         <link rel="preconnect" href="https://gqnwyyinswqoustiqtpk.supabase.co" crossOrigin="anonymous" />
         
-        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon_16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon_32.png" />
         <link rel="icon" type="image/png" sizes="48x48" href="/icons/icon_48.png" />
