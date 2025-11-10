8478bac426bde36ed8ee8caf3dd1bae00446d7e8
Alexey Alexeev <alexeevv-alexey@yandex.ru>
2025-11-10 14:40:53 +0300

    исправил код для метрики

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,36 +96,19 @@ export default function RootLayout({
         <meta name="geo.position" content="55.7558;37.6176" />
         <meta name="ICBM" content="55.7558, 37.6176" />
         
-        {/* Yandex.Metrika counter - только для production домена */}
+        {/* Yandex.Metrika counter */}
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-              // Проверяем домен на сервере
-              if (typeof window !== 'undefined' && window.location.hostname === 'salutgrad.ru') {
-                try {
-                  (function(m,e,t,r,i,k,a){
-                      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                      m[i].l=1*new Date();
-                      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                      k=e.createElement(t),a=e.getElementsByTagName(t)[0];
-                      if (a && a.parentNode) {
-                        k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
-                      }
-                  })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104700931', 'ym');
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104700931', 'ym');
 
-                  ym(104700931, 'init', {
-                    ssr:true, 
-                    webvisor:true, 
-                    clickmap:true, 
-                    ecommerce:"dataLayer", 
-                    accurateTrackBounce:true, 
-                    trackLinks:true
-                  });
-                } catch (error) {
-                  console.warn('Yandex Metrika initialization error:', error);
-                }
-              }
+              ym(104700931, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
             `,
           }}
         />
@@ -138,6 +121,7 @@ export default function RootLayout({
             />
           </div>
         </noscript>
+        {/* /Yandex.Metrika counter */}
         
         {/* Conditional noindex meta tags - только для Vercel */}
         <ConditionalNoIndex />
