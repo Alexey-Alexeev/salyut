70495fbd1a5abd9f9dfe23b554aff872935362c2
Alexey Alexeev <alexeevv-alexey@yandex.ru>
2025-11-11 21:20:54 +0300

    переместил в head

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,29 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
+        {/* Yandex.Metrika counter - размещён в начале head для ранней загрузки */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104700931', 'ym');
+
+              ym(104700931, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/104700931" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+        
         {/* DNS prefetch и preconnect для внешних ресурсов */}
         <link rel="dns-prefetch" href="https://gqnwyyinswqoustiqtpk.supabase.co" />
         <link rel="preconnect" href="https://gqnwyyinswqoustiqtpk.supabase.co" crossOrigin="anonymous" />
@@ -131,33 +154,6 @@ export default function RootLayout({
         <MobileExitBottomSheet />
         <Toaster />
         <CacheBuster />
-
-        {/* Yandex.Metrika counter */}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(m,e,t,r,i,k,a){
-                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                  m[i].l=1*new Date();
-                  for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
-                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
-
-              ym(104700931, 'init', { webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true });
-            `,
-          }}
-        />
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/104700931"
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt=""
-            />
-          </div>
-        </noscript>
-        {/* /Yandex.Metrika counter */}
       </body>
     </html>
   );
