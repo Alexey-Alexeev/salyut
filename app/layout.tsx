202f1657213f572f4b12002aadab4a9cd735c82f
Alexey Alexeev <alexeevv-alexey@yandex.ru>
2025-11-12 02:08:59 +0300

    обновил скрипт

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,22 +102,22 @@ export default function RootLayout({
         
         {/* Глобальная JSON-LD разметка организации для всех страниц */}
         <OrganizationJsonLd />
-
-            {/* ✅ Яндекс.Метрика через next/script */}
-                  <script
+ {/* ✅ Оригинальный код Яндекс.Метрики */}
+ <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(m,e,t,r,i,k,a){
                   m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                   m[i].l=1*new Date();
                   for (var j = 0; j < document.scripts.length; j++) {
-                    if (document.scripts[j].src === r) return;
+                      if (document.scripts[j].src === r) { return; }
                   }
                   k=e.createElement(t),a=e.getElementsByTagName(t)[0];
                   k.async=1;k.src=r;a.parentNode.insertBefore(k,a);
-              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=104700931", "ym");
 
               ym(104700931, "init", {
+                  ssr:true,
                   clickmap:true,
                   trackLinks:true,
                   accurateTrackBounce:true,
@@ -125,16 +125,20 @@ export default function RootLayout({
                   ecommerce:"dataLayer"
               });
 
-              console.log("%c[Яндекс.Метрика] Инициализация выполнена", "color:#00aa00");
+              console.log("%c[Яндекс.Метрика] Счётчик 104700931 инициализирован", "color: #00aa00");
             `,
           }}
         />
 
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<div><img src="https://mc.yandex.ru/watch/104700931" style="position:absolute; left:-9999px;" alt="" /></div>`,
-          }}
-        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/104700931"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </head>
       <body className={inter.className}>
         <div className="flex min-h-screen flex-col">
